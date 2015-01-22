6a46c739be206b441ee0a64d326b6b513951093c
Ben <hetzerbr>
2015-01-22 15:29:06 -0600

    Made skeleton for slide menu —still testing

---
+++ Step It Up/MainVC.h
@@ -0,0 +1,13 @@
+//
+//  MainVC.h
+//  Step It Up
+//
+//  Created by Hetzer, Benjamin Ross on 1/22/15.
+//  Copyright (c) 2015 UWEC Recreation. All rights reserved.
+//
+
+#import "AMSlideMenuMainViewController.h"
+
+@interface MainVC : AMSlideMenuMainViewController
+
+@end
